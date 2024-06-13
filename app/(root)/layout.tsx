f212e341c7c7f0f71090cfe5fd3a14f0a227921f
MasterofGames77 <mgambardella16@gmail.com>
2024-06-12 20:41:36 -0400

    implement appwrite WIP

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,24 +1,26 @@
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn = {
-    firstName: "Michael",
-    lastName: "Gambardella",
-    email: "mgambardella7@email.com",
-  };
+  const loggedIn = await getLoggedInUser();
+
+  if (!loggedIn) redirect("/sign-in");
+  console.log(loggedIn);
+
   return (
     <main className="flex h-screen w-full font-inter">
       <Sidebar user={loggedIn} />
 
       <div className="flex size-full flex-col">
         <div className="root-layout">
-          <Image src="/icons/logo/svg" width={30} height={30} alt="logo" />
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
           <div>
             <MobileNav user={loggedIn} />
           </div>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,12 @@
 import HeaderBox from "@/components/HeaderBox";
 import RightSideBar from "@/components/RightSideBar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const Home = () => {
-  const loggedIn = {
-    firstName: "Michael",
-    lastName: "Gambardella",
-    email: "mgambardella7@email.com",
-  };
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
+
   return (
     <section className="home">
       <div className="home-content">
@@ -16,8 +14,8 @@ const Home = () => {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || "Guest"}
-            subtext="Access and manage your account and transactions effeciently."
+            user={loggedIn?.name || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
           />
 
           <TotalBalanceBox
